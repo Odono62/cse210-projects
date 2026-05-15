286bfc5d9b1064d78b834757b3cc3b64cc030bae
odono62 <odonoj62@gmail.com>
2026-05-15 05:54:43 +0100

    last week assignment revision

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,12 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your first name?");
+        Console.Write("What is your first name? ");
         string firstName = Console.ReadLine();
 
-        Console.WriteLine("What is your last name?");
+        Console.Write("What is your last name? ");
         string lastName = Console.ReadLine();
 
-        Console.WriteLine($"your name is {firstName} {lastName}");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
     }
 }
