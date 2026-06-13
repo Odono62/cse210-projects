21b0d6473e3dbaa4e1874b8efc6fdcfea92427c1
odono62 <odonoj62@gmail.com>
2026-06-13 01:36:42 +0100

    hhhh

---
+++ week05/Homework/reg.cs
@@ -0,0 +1,15 @@
+public class Person
+{
+    public string Getname ()
+    {
+        return "joseph";
+    }
+}
+
+public class Student : Person
+{
+    public string Getnumber()
+    {
+        return "09074204703";
+    }
+}
